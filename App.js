e8ce60bed06e90a2202f8d688c46bfe337d2147b
estellaaaa <estella.kinzel@gmail.com>
2024-01-21 23:54:16 +0100

    First upload

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Habit from './components/habit';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Your Habits for today */}
+      <View style={styles.habitsWrapper}>
+        <Text style={styles.sectionTitle}>Today's Habits</Text>
+
+        <View style={styles.items}>
+          {/* This is where the habits wil go! */}
+          <Habit text={'Habit 1'} />
+          <Habit text={'Habit 2'} />
+        </View>
+
+      </View>
+
     </View>
   );
 }
@@ -13,8 +24,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#2E2929',
   },
+
+  habitsWrapper: {
+    paddingTop: 20,
+    paddingHorizontal: 30,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  items: {
+    marginTop: 30,
+  }
 });
